8bbaa56c20c6791bb84ea43eb9fd4a5e1d59a159
Henil <henilp467@gmail.com>
2024-12-22 18:44:16 +1100

    [signup-test.rb]: use find_element of SearchContext class and use the 'id' locator to find all HTML field elements in the Selenium Blog web app's signup page

--- Tests/signup-test.rb
+++ Tests/signup-test.rb
@@ -8,5 +8,11 @@ driver.navigate.to "https://selenium-blog.herokuapp.com/signup" # the driver nav
 # Step 2: after creating a driver, it's necessary to search for elements in the webpage before interacting with them and performing actions
 # find_element is the primary method of the SearchContext class
 
+# Use the "id" locator to find the element on the web page
+username_field = driver.find_element(id: "user_username")
+email_field = driver.find_element(id: "user_email")
+password_field = driver.find_element(id: "user_password")
+submit_button = driver.find_element(id: "submit")
+
 # Step 3: close out the driver session
 driver.quit
