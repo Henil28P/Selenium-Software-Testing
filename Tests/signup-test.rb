8c99fd8d207f1712dd6360ffd340576c54298f1a
Henil <henilp467@gmail.com>
2024-12-22 18:47:47 +1100

    [signup-test.rb]: use send_keys method to drive actions on the all elements found (username, email, password, and sign-up button)

--- Tests/signup-test.rb
+++ Tests/signup-test.rb
@@ -9,10 +9,19 @@ driver.navigate.to "https://selenium-blog.herokuapp.com/signup" # the driver nav
 # find_element is the primary method of the SearchContext class
 
 # Use the "id" locator to find the element on the web page
+# 2. Drive actions on the above elements found - send keys to the username, email and password fields and finally finish by clicking the 'submit' button
+
 username_field = driver.find_element(id: "user_username")
+username_field.send_keys("user")
+
 email_field = driver.find_element(id: "user_email")
+email_field.send_keys("email@test.com")
+
 password_field = driver.find_element(id: "user_password")
+password_field.send_keys("password")
+
 submit_button = driver.find_element(id: "submit")
+submit_button.click
 
 # Step 3: close out the driver session
 driver.quit
