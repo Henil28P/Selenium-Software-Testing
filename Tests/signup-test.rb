39bac97ab4e52442e6e88b5ba5bdf45918c2d786
Henil <henilp467@gmail.com>
2024-12-24 11:34:37 +1100

    [signup-test.rb]: clean signup test code with functions to prevent repetiveness of actions

--- Tests/signup-test.rb
+++ Tests/signup-test.rb
@@ -11,6 +11,40 @@ email = "user#{timestamp}@test.com"
 password = "password"
 expected_banner_text = "Welcome to the alpha blog user #{timestamp}"
 
+# It's necessary to search for elements in the webpage before interacting with them and performing actions
+# find_element is the primary method of the SearchContext class
+
+# Use the "id" locator to find the element on the web page
+# 2. Drive actions on the above elements found - send keys to the username, email and password fields and finally finish by clicking the 'submit' button
+
+# methods for better readability and produce clean test code
+def enter_username(username):
+    username_field = driver.find_element(id: "user_username")
+    username_field.send_keys(username)
+end
+
+def enter_email(email):
+    email_field = driver.find_element(id: "user_email")
+    email_field.send_keys(email)
+end
+
+def enter_password(password):
+    password_field = driver.find_element(id: "user_password")
+    password_field.send_keys(password)
+end
+
+def submit_form()
+    submit_button = driver.find_element(id: "submit")
+    submit_button.click
+end
+
+
+def get_banner_text()
+    # this will return a text value
+    banner = driver.find_element(id: "flash_success")
+    banner_text = banner.text # to grab the text from the banner element
+end
+
 # TEST: Sign up for blog
 # To use rspec-core, add a 'describe' block which describes what the test is doing (describing a blog application for this project)
 describe "Blog application" do
@@ -24,28 +58,14 @@ describe "Blog application" do
 
             driver.navigate.to "https://selenium-blog.herokuapp.com/signup" # the driver navigates to the webpage link of the Heroku app passed to it
 
-            # Step 2: after creating a driver, it's necessary to search for elements in the webpage before interacting with them and performing actions
-            # find_element is the primary method of the SearchContext class
-
-            # Use the "id" locator to find the element on the web page
-            # 2. Drive actions on the above elements found - send keys to the username, email and password fields and finally finish by clicking the 'submit' button
-
-            username_field = driver.find_element(id: "user_username")
-            username_field.send_keys(username)
-
-            email_field = driver.find_element(id: "user_email")
-            email_field.send_keys(email)
-
-            password_field = driver.find_element(id: "user_password")
-            password_field.send_keys(password)
-
-            submit_button = driver.find_element(id: "submit")
-            submit_button.click
+            enter_username(username)
+            enter_email(email)
+            enter_password(password)
+            submit_form()
 
             # Choose the expectation that the user's account was created successfully after form submission
             # --> Get the message from the 'banner' element which confirms that the user is signed up
-            banner = driver.find_element(id: "flash_success")
-            banner_text = banner.text # to grab the text from the banner element
+            banner_text = get_banner_text()
             expect(banner_text).to eq(expected_banner_text) # to expect the actual banner text equals the expected banner text
 
             # Step 3: close out the driver session
