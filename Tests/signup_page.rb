7ab52670077a6c21dbfa58141cd78eab5661c664
Henil <henilp467@gmail.com>
2024-12-24 12:54:16 +1100

    [signup_page.rb]: assign values for each CSS selector constant variables as a Hashmap and use these constants

--- Tests/signup_page.rb
+++ Tests/signup_page.rb
@@ -1,10 +1,11 @@
 class SignupPage
 
+    # assign values for each of the CSS selector constants as a Hashmap
 # 1. CSS selectors - declare all CSS selectors as constants with descriptive names that represent each element
-USERNAME_FIELD
-EMAIL_FIELD
-PASSWORD_FIELD
-SUBMIT_BUTTON
+USERNAME_FIELD = {id: "user_username"}
+EMAIL_FIELD = {id: "user_email"}
+PASSWORD_FIELD = {id: "user_password"}
+SUBMIT_BUTTON = {id: "submit"}
 
 # attr_reader to allow the webdriver instance variable to be created in the test and used throughout the class
 attr_reader :driver # takes a driver object
@@ -16,22 +17,22 @@ end
 
 # methods for better readability and produce clean test code
 def enter_username(username):
-    username_field = @driver.find_element(id: "user_username")
+    username_field = @driver.find_element(USERNAME_FIELD)
     username_field.send_keys(username)
 end
 
 def enter_email(email):
-    email_field = @driver.find_element(id: "user_email")
+    email_field = @driver.find_element(EMAIL_FIELD)
     email_field.send_keys(email)
 end
 
 def enter_password(password):
-    password_field = @driver.find_element(id: "user_password")
+    password_field = @driver.find_element(PASSWORD_FIELD)
     password_field.send_keys(password)
 end
 
 def submit_form()
-    submit_button = @driver.find_element(id: "submit")
+    submit_button = @driver.find_element(SUBMIT_BUTTON)
     submit_button.click
 end
 
